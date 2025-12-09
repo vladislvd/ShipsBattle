9c98a2b081c414e852f7e82dfeb45525233b549c
vladislvd <vedolmatov@mail.ru>
2025-12-09 17:05:39 +0300

    add ships for players

---
+++ objects/ship.py
@@ -0,0 +1,24 @@
+import objects
+import config
+
+
+class ShipsDrawer:
+    def __init__(self, max_long, batch, start_x, start_y):
+        self.ships = []
+        self.start_x = start_x
+        self.start_y = start_y
+        for long in range(1, max_long+1):
+            x_for_ships_in_row = self.start_x
+            for ship_num in range(max_long-long+1):
+                ship = []
+                for deck_num in range(long):
+                    deck = objects.Cell(
+                        x=x_for_ships_in_row + deck_num*(config.CELL_SIZE+config.BORDER_SIZE),
+                        y=self.start_y,
+                        type=1,
+                        batch=batch
+                    )
+                    ship.append(deck)
+                self.ships.append(ship)
+                x_for_ships_in_row = self.ships[-1][-1].x + config.CELL_SIZE + config.BORDER_SIZE*4
+            self.start_y -= config.CELL_SIZE + config.BORDER_SIZE*4
